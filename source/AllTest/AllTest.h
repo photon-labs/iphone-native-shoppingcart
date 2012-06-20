80139584e8f60cc156058851a9b4efffa2821a20
Sathiyasuresh <sathiyasuresh.pandian@photoninfotech.net>
2012-06-20 19:08:03 +0530

    Alltest files included

---
+++ source/AllTest/AllTest.h
@@ -0,0 +1,13 @@
+//
+//  AllTest.h
+//  AllTest
+//
+//  Created by Administrator on 20/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AllTest : SenTestCase
+
+@end
