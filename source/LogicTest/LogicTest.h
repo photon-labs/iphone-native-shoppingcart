deb4f84ded6bba5b52f4cd7dc51234cb3997b7fb
Sathiyasuresh <sathiyasuresh.pandian@photoninfotech.net>
2012-10-05 20:57:48 +0530

    "[artf545350 ]-iPhone Native Eshop issues addressed"

--- source/LogicTest/LogicTest.h
+++ source/LogicTest/LogicTest.h
@@ -7,7 +7,44 @@
 //
 
 #import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIkit.h>
 
-@interface LogicTest : SenTestCase
+#import "PhrescoAppDelegate.h"
+#import "RootViewController.h"
+#import "HomeViewController.h"
+#import "BrowseViewController.h"
+#import "ResultViewController.h"
+#import "ProductDetailsViewController.h"
+#import "AddToBagViewController.h"
+#import "ViewCartController.h"
+#import "CheckOutViewController.h"
+#import "CheckOutOVerallViewController.h"
+#import "ReviewViewController.h"
+#import "ReviewCommentsViewController.h"
+#import "LoginViewController.h"
+#import "RegistrationViewController.h"
+#import "SpecialOffersViewController.h"
+@interface LogicTest : SenTestCase {
+    
+@private
+    iShopAppDelegate *appDelegate;
+    RootViewController *rootController;
+    HomeViewController *homeController;
+    BrowseViewController *browseController;
+    ResultViewController *resultController;
+    ProductDetailsViewController *pdtDetailController;
+    AddToBagViewController *addCartController;
+    ViewCartController *viewCartController;
+    CheckOutViewController *checkViewController;
+    CheckOutOVerallViewController *checkOverallController;
+    ReviewViewController *reviewController;
+    ReviewCommentsViewController *reviewCommentsController;
+    LoginViewController *loginController;
+    RegistrationViewController *registerController;
+    SpecialOffersViewController *splOfferController;
+    
+    UITableView *tblView;
+
+}
 
 @end
--- source/LogicTest/LogicTest.m
+++ source/LogicTest/LogicTest.m
@@ -26,7 +26,85 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in LogicTest");
+    [rootController tabBarButtonAction:@"0"];
+    // STFail(@"Unit tests are not implemented yet in HomeViewTest");
+}
+-(void) testAction
+{
+    [homeController buttonAction:@"0"];
+    
+}
+-(void) testBrowse
+{
+    
+    //[browseController  tableView:tblView didSelectRowAtIndexPath:0];
+    
+    STAssertThrows([browseController tableView:tblView didSelectRowAtIndexPath:-1] , @"Invalid index");
+    
+    
+}
+-(void) testProductResult
+{
+    [resultController  tableView:tblView didSelectRowAtIndexPath:0];
+    [resultController reviewButtonSelected:@"0"];
+    
+}
+-(void) testProductDetail
+{
+    [pdtDetailController addToCart:@"0"];
+    
+    
+}
+
+
+-(void) testAddToCart
+{
+    [addCartController removeIndex:@"0"];
+    
+    
+}
+-(void) testViewCart
+{
+    [viewCartController browseButtonSelected:@"0"];
+    
+}
+-(void) testCheckCart
+{
+    [checkViewController cancelAction:@"0"];
+    
+}
+-(void) testCheckOverall
+{
+    [checkViewController reviewAction:@"0"];
+    
+}
+-(void) testReview
+{
+    [reviewController tableView:tblView didSelectRowAtIndexPath:0];
+    
+}
+-(void) testComments
+{
+    [reviewCommentsController goBack:0];
+    
+}
+
+-(void) testLogin
+{
+    [loginController registerButtonSelected:0];
+    
+}
+
+-(void) testRegister
+{
+    [registerController registerButtonSelected:0];
+    
+}
+
+-(void) testSplOffers
+{
+    [splOfferController tableView:tblView didSelectRowAtIndexPath:0];
+    
 }
 
 @end
