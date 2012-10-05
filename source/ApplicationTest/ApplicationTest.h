cfdbd8d86f163e2cce4ba3ddf7515ba4d28e5908
Sathiyasuresh <sathiyasuresh.pandian@photoninfotech.net>
2012-10-05 11:29:15 +0530

    "[artf540079]-Application unit test included"

---
+++ source/ApplicationTest/ApplicationTest.h
@@ -0,0 +1,13 @@
+//
+//  ApplicationTest.h
+//  ApplicationTest
+//
+//  Created by Sathiyasuresh on 05/10/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ApplicationTest : SenTestCase
+
+@end
---
+++ source/ApplicationTest/ApplicationTest.m
@@ -0,0 +1,32 @@
+//
+//  ApplicationTest.m
+//  ApplicationTest
+//
+//  Created by Sathiyasuresh on 05/10/12.
+//
+//
+
+#import "ApplicationTest.h"
+
+@implementation ApplicationTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ApplicationTest");
+}
+
+@end
